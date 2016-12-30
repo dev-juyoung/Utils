25ab8f21c7ad3709b36de5c83017d088e9a076d4
jay <jay.chang@redso.com.hk>
2016-12-30 11:42:29 +0800

    no message

--- library/src/main/java/com/jaychang/utils/StringUtils.java
+++ library/src/main/java/com/jaychang/utils/StringUtils.java
@@ -39,4 +39,75 @@ public class StringUtils {
     return positions;
   }
 
+  public static String capitalize(final String str, final char... delimiters) {
+    final int delimLen = delimiters == null ? -1 : delimiters.length;
+    if (StringUtils.isEmpty(str) || delimLen == 0) {
+      return str;
+    }
+    final char[] buffer = str.toCharArray();
+    boolean capitalizeNext = true;
+    for (int i = 0; i < buffer.length; i++) {
+      final char ch = buffer[i];
+      if (isDelimiter(ch, delimiters)) {
+        capitalizeNext = true;
+      } else if (capitalizeNext) {
+        buffer[i] = Character.toTitleCase(ch);
+        capitalizeNext = false;
+      }
+    }
+    return new String(buffer);
+  }
+
+  public static String capitalize(final String str) {
+    return capitalize(str, null);
+  }
+
+  public static String capitalizeFully(String str, final char... delimiters) {
+    final int delimLen = delimiters == null ? -1 : delimiters.length;
+    if (StringUtils.isEmpty(str) || delimLen == 0) {
+      return str;
+    }
+    str = str.toLowerCase();
+    return capitalize(str, delimiters);
+  }
+
+  public static String capitalizeFully(String str) {
+    return capitalizeFully(str, null);
+  }
+
+  public static String uncapitalize(final String str, final char... delimiters) {
+    final int delimLen = delimiters == null ? -1 : delimiters.length;
+    if (StringUtils.isEmpty(str) || delimLen == 0) {
+      return str;
+    }
+    final char[] buffer = str.toCharArray();
+    boolean uncapitalizeNext = true;
+    for (int i = 0; i < buffer.length; i++) {
+      final char ch = buffer[i];
+      if (isDelimiter(ch, delimiters)) {
+        uncapitalizeNext = true;
+      } else if (uncapitalizeNext) {
+        buffer[i] = Character.toLowerCase(ch);
+        uncapitalizeNext = false;
+      }
+    }
+    return new String(buffer);
+  }
+
+  public static String uncapitalize(final String str) {
+    return uncapitalize(str, null);
+  }
+
+  private static boolean isDelimiter(final char ch, final char[] delimiters) {
+    if (delimiters == null) {
+      return Character.isWhitespace(ch);
+    }
+    for (final char delimiter : delimiters) {
+      if (ch == delimiter) {
+        return true;
+      }
+    }
+    return false;
+  }
+
 }
