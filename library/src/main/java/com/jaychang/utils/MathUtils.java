4aef795eb12f6e11c3038737ce1f3a1c0caf7554
jay <jay.chang@redso.com.hk>
2017-04-26 18:47:09 +0800

    no message

---
+++ library/src/main/java/com/jaychang/utils/MathUtils.java
@@ -0,0 +1,13 @@
+package com.jaychang.utils;
+
+public class MathUtils {
+
+  public static float round(float number, int scale) {
+    int pow = 10;
+    for (int i = 1; i < scale; i++)
+      pow *= 10;
+    float tmp = number * pow;
+    return (float) (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) / pow;
+  }
+
+}
