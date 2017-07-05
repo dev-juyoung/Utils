342ea753238ba586baad4ed7f2fe97985eafece5
jay <jay.chang@redso.com.hk>
2017-07-05 17:23:16 +0800

    no message

--- library/src/main/java/com/jaychang/utils/StringUtils.java
+++ library/src/main/java/com/jaychang/utils/StringUtils.java
@@ -110,4 +110,12 @@ public class StringUtils {
     return false;
   }
 
+  public static String withSuffix(long count) {
+    if (count < 1000) return "" + count;
+    int exp = (int) (Math.log(count) / Math.log(1000));
+    return String.format("%.1f %c",
+      count / Math.pow(1000, exp),
+      "kMGTPE".charAt(exp-1));
+  }
+
 }
