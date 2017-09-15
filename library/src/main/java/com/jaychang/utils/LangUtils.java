96fda381416a0341a0c14ad03720d2b5c01b30f1
jay <jay.chang@redso.com.hk>
2017-09-15 10:58:09 +0800

    no message

--- library/src/main/java/com/jaychang/utils/LangUtils.java
+++ library/src/main/java/com/jaychang/utils/LangUtils.java
@@ -1,9 +1,13 @@
 package com.jaychang.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.Build;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import java.util.Locale;
@@ -15,41 +19,54 @@ public class LangUtils {
   private static final String KEY_CURRENT_LANG = LangUtils.class.getName() + "_CURRENT_LANG";
   private static final String KEY_CURRENT_COUNTRY = LangUtils.class.getName() + "_KEY_CURRENT_COUNTRY";
 
-  private static void changeLanguage(Context context, Locale locale, boolean restart) {
-    PreferenceUtils.saveString(context, KEY_CURRENT_LANG, locale.getLanguage());
-    PreferenceUtils.saveString(context, KEY_CURRENT_COUNTRY, locale.getCountry());
-
-    Locale.setDefault(locale);
-    Context appContext = context.getApplicationContext();
-    Resources resources = appContext.getResources();
-    Configuration config = new Configuration(resources.getConfiguration());
-    config.locale = locale;
-    resources.updateConfiguration(config, resources.getDisplayMetrics());
-    if (restart) {
-      Intent refresh = getLauncherIntent(context);
-      refresh.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-      context.startActivity(refresh);
-    }
-  }
-
   public static void changeLanguage(Context context, Locale locale) {
-    changeLanguage(context, locale, true);
+    saveString(context, KEY_CURRENT_LANG, locale.getLanguage());
+    saveString(context, KEY_CURRENT_COUNTRY, locale.getCountry());
+
+    wrap(context);
+
+    Intent refresh = getLauncherIntent(context);
+    refresh.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    context.startActivity(refresh);
   }
 
-  public static void restoreLanguage(Context context) {
-    changeLanguage(context, getLocale(context), false);
+  public static Context wrap(Context context) {
+    Locale locale = getLocale(context);
+    Locale.setDefault(locale);
+
+    Resources res = context.getResources();
+    Configuration config = new Configuration(res.getConfiguration());
+    if (Build.VERSION.SDK_INT >= 17) {
+      config.setLocale(locale);
+      context = context.createConfigurationContext(config);
+    } else {
+      config.locale = locale;
+      res.updateConfiguration(config, res.getDisplayMetrics());
+    }
+    return context;
   }
 
   private static Locale getLocale(Context context) {
     Locale result;
-    String lang = PreferenceUtils.getString(context, KEY_CURRENT_LANG);
-    String country = PreferenceUtils.getString(context, KEY_CURRENT_COUNTRY);
+    String lang = getString(context, KEY_CURRENT_LANG);
+    String country = getString(context, KEY_CURRENT_COUNTRY);
     if (!TextUtils.isEmpty(lang)) {
       result = new Locale(lang, country);
     } else {
       result = Locale.getDefault();
     }
     return result;
+  }
+
+  @SuppressLint("ApplySharedPref")
+  private static void saveString(Context context, String key, String value) {
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+    prefs.edit().putString(key, value).commit();
+  }
+
+  private static String getString(Context context, String key) {
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+    return prefs.getString(key, "");
   }
 
 }
